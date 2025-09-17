d6b523f992a4ba8784c778d8313ac1050202df10
Romina Martínez <romina.martinez496@gmail.com>
2025-09-17 19:37:33 -0300

    Update login.ts

--- login/login.ts
+++ login/login.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+  loginObj: any = {
+    userName: '',
+    password: '',
 
+  };
+router = inject(Router);
+
+  onLogin() {
+    if(this.loginObj.userName == 'admin' && this.loginObj.password == '11223')
+      {this.router.navigateByUrl('/dashboard')
+  } else {
+alert('Credenciales incorrectas ✋🏽')
+}
+
+}
 }
